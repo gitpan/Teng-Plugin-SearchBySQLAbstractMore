07c48884e9682bc71c3a06c79a7217f2d31dc05e
加藤敦 <ktat@cpan.org>
2012-01-07 05:20:16 +0000

    Import of KTAT/Teng-Plugin-SearchBySQLAbstractMore-0.04 from CPAN.
    gitpan-cpan-distribution: Teng-Plugin-SearchBySQLAbstractMore
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         KTAT/Teng-Plugin-SearchBySQLAbstractMore-0.04.tar.gz
    gitpan-cpan-author:       KTAT
    gitpan-cpan-maturity:     released

--- lib/Teng/Plugin/SearchBySQLAbstractMore.pm
+++ lib/Teng/Plugin/SearchBySQLAbstractMore.pm
@@ -149,11 +149,11 @@ Teng::Plugin::SearchBySQLAbstractMore - use SQL::AbstractMore as Query Builder f
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
